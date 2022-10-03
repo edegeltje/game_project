d1c4841ba0e23d9e6290a09819bb9c1a0fae49bc
Blizzard_inc <thrashy4inbox@gmail.com>
2022-10-03 16:31:36 +0200

    Model start

--- app/Model.hs
+++ app/Model.hs
@@ -1,4 +1,49 @@
 module Model where
 
+import Data.Map
+
 foo :: IO ()
 foo = putStrLn "foo function in Model"
+
+data GameState = Gamestate {
+    menustate :: MenuState,
+    maze :: Maze,
+    score :: Score
+    }
+
+data MenuState = PauseMenu | StartMenu | Playing
+
+type Score = Int
+
+type Maze = Map Location (Maybe Entity)
+
+data Location = MkLocation {xposition:: Int,yposition::Int}
+  deriving (Eq, Ord)
+
+data Direction = North | South | East | West 
+
+data Entity = Agent | Collectible | Wall
+
+class Positioned a where
+  getPosition :: a -> Location
+  setPosition :: a -> Location -> a
+
+class (Positioned a) => Agent a where
+  getDirection :: a -> Direction
+  setDirection :: a -> Direction -> a
+
+
+
+data PowerState = PoweredUp | Weak
+
+data PlayerEntity = MkPlayer {
+  location :: Location,
+  movementDirection :: Direction,
+  powerState :: PowerState
+}
+
+
+
+data EnemyEntity = MkEnemy {
+
+}
