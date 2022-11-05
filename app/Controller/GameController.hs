faa73ae1d13818616960494d1549d8086b0064c8
MennoBrl <51479931+MennoBrl@users.noreply.github.com>
2022-11-05 19:07:36 +0100

    Picking up dots and showing score

--- app/Controller/GameController.hs
+++ app/Controller/GameController.hs
@@ -5,6 +5,7 @@ import Model.Entities
 import Controller.MenuController
 import View.StaticSprites
 import Model.Settings (tILESIZE, Settings (gameSpeed))
+import Data.Map
 
 inputFromButton :: GameState -> InputButton -> IO GameState
 inputFromButton gs ib = return gs {inputBuffer = ib}
@@ -14,7 +15,10 @@ newTime gs dt = time gs + dt * gameSpeed (settings gs)
 
 step :: GameState -> Float -> IO GameState
 step gs@MkGameState{inputBuffer = InputNeutral} dt | floor (time gs) == floor (newTime gs dt) =
-  return gs {time = newTime gs dt}-- don't update the model every frame, just every "second"
+  return gs {time = newTime gs dt, entities = newEntities, maze = newMaze, score = score gs + scoreD1 + scoreD2}-- don't update the model every frame, just every "second"
+  where
+      (newEntities,scoreD1) = updateEntities gs
+      (newMaze,scoreD2) = updateMaze gs newEntities
 step gs@MkGameState{inputBuffer = InputBack} dt = return
   gs {
     inputBuffer = InputNeutral,
@@ -25,28 +29,36 @@ step gs@MkGameState{ entities = currentEntities@MkEntityRecord{player = currentP
     | dirPossible gs (getPosition currentPlayer) (inputToDir (inputBuffer gs))
       = return gs {
         time = newTime gs dt,
-        entities = currentEntities{
-          player = currentPlayer{
+        entities = newEntities{player = currentPlayer{
             playerPosition = calcNewPosition
               (playerPosition currentPlayer)
               (inputToDir (inputBuffer gs)),
             playerMovementDirection = inputToDir (inputBuffer gs)
-            }
-          }
+            }},
+        maze = newMaze,
+        score = score gs + scoreD1 + scoreD2
         } --This makes pacman move in the direction in the inputbuffer if it is possible
     | dirPossible gs (getPosition currentPlayer) (getDirection currentPlayer)  = return
       gs {
         time = newTime gs dt,
-        entities = currentEntities{
+        entities = newEntities{
           player = currentPlayer{
             playerPosition = calcNewPosition
               (playerPosition currentPlayer)
               (playerMovementDirection currentPlayer)
             }
-          }
+          },
+        maze = newMaze,
+        score = score gs + scoreD1 + scoreD2
         } --This makes pacman move in the current direction if the direction in the input buffer is not possible
-    | otherwise = return gs {time = newTime gs dt}
+    | otherwise = return gs {time = newTime gs dt,
+    entities = newEntities,
+    maze = newMaze,
+    score = score gs + scoreD1 + scoreD2}
     --This makes sure that pacman stops moving when he hits a wall
+    where
+      (newEntities,scoreD1) = updateEntities gs
+      (newMaze,scoreD2) = updateMaze gs newEntities
 
 -- step gs dt =
 --   return gs{
@@ -88,10 +100,30 @@ inputToDir InputDown = South
 inputToDir _ = North
 
 
--- updateEntities :: GameState -> (EntityRecord,Score)
--- updateEntities = undefined
--- updateMaze :: GameState -> EntityRecord -> (BottomLayer,Score)
--- updateMaze = undefined
+updateEntities :: GameState -> (EntityRecord,Score)
+updateEntities gs@MkGameState{entities = currentEntities@MkEntityRecord{player = currentPlayer}} 
+              | getPositionContent (playerPosition currentPlayer) (maze gs) == PowerDot 
+                = ((entities gs){enemies = Prelude.map scare (enemies (entities gs))}, currentScore)
+  where
+    scare enemies = enemies{enemyStatus = Scared}
+    currentScore = score gs
+    
+updateEntities gs = (entities gs, score gs)
+
+updateMaze :: GameState -> EntityRecord -> (BottomLayer,Score)
+updateMaze gs er@MkEntityRecord{player = currentPlayer} 
+          | getPositionContent (playerPosition currentPlayer) (maze gs) == PowerDot 
+            = (insert (playerPosition currentPlayer) Empty (maze gs) , currentScore + 50)
+  where
+    currentScore = score gs
+
+updateMaze gs er@MkEntityRecord{player = currentPlayer} 
+          | getPositionContent (playerPosition currentPlayer) (maze gs) == SmallDot
+            = (insert (playerPosition currentPlayer) Empty (maze gs) , currentScore + 10)
+  where
+    currentScore = score gs
+
+updateMaze gs er = (maze gs, score gs)
 
 calcNewPosition :: Position -> Direction -> Position
 calcNewPosition (x,y) North = (x,y + 1)
